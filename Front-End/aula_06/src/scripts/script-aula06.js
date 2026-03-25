b6269a7ec5d182875dc7a16971b1da82cdb97d06
Yasmin-Vitoria77 <yasmin.v.ramos@aluno.senai.br>
2026-03-25 14:27:24 -0300

    Scripts do arquivo da aula 6 - exercícios de alteração

---
+++ Front-End/aula_06/src/scripts/script-aula06.js
@@ -0,0 +1,21 @@
+// const clique = document.querySelector('#contador')
+// const botao = document.querySeelector('#btn-pedir')
+
+// clique.addEventListener('click', function() {
+//     const converter  = 
+// })
+// NÃO SEI FAZER
+
+const escrita = document.querySelector ('.card-acao')
+escrita.addEventListener('keyup', () => {
+    escrita.value
+})
+
+const caixa = document.querySelector('#caixa-cor')
+caixa.addEventListener('mouseover', () => {
+    caixa.style.backgroundColor = 'blue';
+})
+
+caixa.addEventListener('mouseout', () => {
+    caixa.style.backgroundColor = 'grey';
+})
