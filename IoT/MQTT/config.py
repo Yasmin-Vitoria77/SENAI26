7488aa03895c5e46bcfcf377d121e7003ca0a7b3
Yasmin-Vitoria77 <yasmin.v.ramos@aluno.senai.br>
2026-06-24 15:44:27 -0300

    Abelha

--- IoT/MQTT/config.py
+++ IoT/MQTT/config.py
@@ -2,7 +2,7 @@
 
 WIFI_SSID = "WIFI_IOT" # nome da rede WiFi da sala
 WIFI_PASS = "Ac1ce2ss5@IOT" # senha da rede (ajuste conforme necessário)
-BROKER_IP = "10.132.106.28" # IP do seu notebook (veja com ipconfig)
+BROKER_IP = "10.132.112.5" # IP do seu notebook (veja com ipconfig)
 # -> Máquina do Marlon é o Broker - IP da máquina dele
 BROKER_PORT = 1883
 CLIENT_ID = "yasmin_min" # identificador único do seu dispositivo
